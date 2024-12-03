9917ce60e43272545f9e71ab7e669ab18ad1a7c6
Brutos212 <Brutos212@gmail.com>
2024-12-03 20:33:31 -0300

    Primeiro commit desta DashBord

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Html Tutorial',
+        productNumber: '923413',
+        paymentStatus: 'Studying',
+        status: 'Active'
+    },
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '87543',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97543',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '823413',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
